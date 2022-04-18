cd271a6803c2cb7b7b13407ea90786dfe65cca3a
Leonardo Cortes <lcortes@joonik.com>
2022-04-18 16:45:11 -0500

    first commit

---
+++ replace.js
@@ -0,0 +1,6 @@
+var cadena = `On the weekends, Lucy and Millie come to London and the whole family has lunch together. Sometimes their aunts and uncles come, too. Shawn’s grandparents are very old, and they live in a small cottage in Plymouth. They’re his mother’s parents. They’re retired, and spend most of their time gardening, walking and having tea with their friends. His father’s parents died years ago. They used to live in Brixton, in South London. Shawn doesn’t remember much about them. Once a year, Shawn goes to spend some time with his grandparents in Plymouth. His grandmother makes excellent fruit pies. Sometimes, his sisters or his cousins come, too, and they all have a great time. On holidays, Shawn goes to Italy and they stay with Maria’s parents in Rome. He loves Rome, and he loves Italian food, but he doesn’t like staying with his in-laws. He doesn’t speak much Italian, and they don’t speak any English, so it’s a bit difficult to communicate. In the future, Shawn hopes to find time to learn more Italian.`;
+
+
+console.log(cadena.replace(/[’]/g, ""));
+
+
